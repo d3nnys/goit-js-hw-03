67fb7dde7c85c4bb82f3b8b0139c9ea1dfae068a
Denys Puzikov <denys.puzikov@gmail.com>
2023-12-19 23:14:27 +0100

    full hw

---
+++ js/task-2.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function makeArray(firstArray, secondArray, maxLength) {
+  for (let i = 0; i < secondArray.length; i++) {
+    firstArray.push(secondArray[i]);
+
+    if (firstArray.length > maxLength) {
+      return firstArray.slice(0, maxLength);
+    }
+  }
+  return firstArray;
+}
+
+console.log(makeArray(['Mango', 'Poly'], ['Ajax', 'Chelsea'], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(['Mango', 'Poly', 'Houston'], ['Ajax', 'Chelsea'], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(['Mango'], ['Ajax', 'Chelsea', 'Poly', 'Houston'], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus'], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(['Earth', 'Jupiter'], ['Neptune', 'Uranus', 'Venus'], 0)); // []
